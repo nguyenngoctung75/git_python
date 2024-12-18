b4dffe74f224f1b1f7c8f017658a4becf59dd27c
nguyenngoctung75 <tungit1huster@gmail.com>
2024-12-18 18:22:41 +0700

    update

--- app.py
+++ app.py
@@ -14,58 +14,109 @@ nlp = spacy.load("en_core_web_md")
 with open('trainData.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
-patterns = []
-responses = []
-intents = []
-D = 0
+patterns = [item['patterns'] for item in data]
+responses = [item['responses'] for item in data]
+intents = [item['intent'] for item in data]
 
-for item in data:
-    for pattern in item['patterns']:
-        patterns.append(pattern)
-        intents.append(item['intent'])
-        responses.append(item['responses'])
-        D += 1
+D = 0 # number of documents
+for item1 in patterns:
+    D += len(item1)
 
+def lemmatize_sentence(sentence):
+    doc = nlp(sentence)
+    return [token.lemma_ for token in doc]
 
-# Xử lý dữ liệu
+# lemmatize function
 def lemmatize_patterns(patterns):
     lemmatized = []
-    for sentence in patterns:
-        if isinstance(sentence, list):
-            sentence = ' '.join(sentence)
-        doc = nlp(sentence)
-        lemmatized.append([token.lemma_ for token in doc if not token.is_stop])
+    for pattern in patterns:
+        tmp = []
+        for sentence in pattern:
+            tmp.append(lemmatize_sentence(sentence))
+
+        lemmatized.append(tmp)
     return lemmatized
 
 patterns = lemmatize_patterns(patterns)
-vocabulary = list(set(word for pattern in patterns for word in pattern))
 
-idf_arr = [math.log(D / sum(1 for pattern in patterns if word in pattern)) for word in vocabulary]
+# set vocabulary
+vocabulary = {word for item1 in patterns for item2 in item1 for word in item2}
+# print(vocabulary)
 
-def cal_tf_idf(document):
-    tf_arr = [document.count(word) / len(document) for word in vocabulary]
-    return [tf * idf for tf, idf in zip(tf_arr, idf_arr)]
+def vectorized(list_word):
+    tmp = []
+    size = len(list_word)
+    for word in vocabulary:
+        tf = list_word.count(word)/size
+        cnt = sum(1 for item1 in patterns for item2 in item1 if word in item2)
+        idf = math.log(D/(cnt+1))
+        tmp.append(tf*idf)
+    return tmp
 
-tf_idf_all = [cal_tf_idf(pattern) for pattern in patterns]
+patterns_vectorized = []
+for item1 in patterns:
+    tmp = []
+    for item2 in item1:
+        tmp.append(vectorized(item2))
+    patterns_vectorized.append(tmp)
 
-def cal_p_Ck(intent):
-    return intents.count(intent) / len(intents)
+def p_Ck(i):
+    return len(patterns[i]) / D
 
-def cal_p_d_k(word, intent):
-    sum_word = sum(tf_idf_all[idx][vocabulary.index(word)] for idx, item in enumerate(intents) if item == intent and word in vocabulary)
-    sum_intent = sum(sum(tf_idf_all[idx]) for idx, item in enumerate(intents) if item == intent)
-    return (sum_word + 1) / (sum_intent + len(vocabulary))
+def p_dk(i, k):
+    sum_xid = sum(vt[k] for vt in patterns_vectorized[i])
+    all_xi = sum(sum(vt) for vt in patterns_vectorized[i])
+
+    return (1e-06 + sum_xid)/(all_xi + 1e-06 * len(vocabulary))
 
 def get_intent(user_input):
-    doc = nlp(user_input)
-    user_words = [token.lemma_ for token in doc if not token.is_stop and token.lemma_ in vocabulary]
-    probabilities = [(intent, math.log(cal_p_Ck(intent)) + sum(math.log(cal_p_d_k(word, intent)) for word in user_words)) for intent in set(intents)]
-    return max(probabilities, key=lambda x: x[1])[0]
+    user_input = lemmatize_sentence(user_input)
+    input_vector = vectorized(user_input)
+    # print(input_vector)
+
+    if sum(input_vector) == 0: 
+        return None
+
+    probabilities = []
+    i = 0
+    for intent in intents:
+        k = 0
+        tmp = 0
+        for xd in input_vector:
+            pdk = p_dk(i,k)
+            tmp += xd*math.log(pdk)
+            k += 1
+        pck = p_Ck(i)
+        pck_x = 10 ** ( math.log(pck) + tmp )
+        probabilities.append((intent, pck_x))
+        i += 1
+
+    predict_intent = max(probabilities, key=lambda x:x[1])[0]
+    # print(predict_intent)
+    return predict_intent
 
 def get_response(user_input):
-    res_intent = get_intent(user_input)
-    idx = intents.index(res_intent)
-    return random.choice(responses[idx])
+    predict_intent = get_intent(user_input)
+    if predict_intent:
+        if predict_intent == "size":
+            return fh.determine_size(user_input)
+        
+        i = intents.index(predict_intent)
+        response = random.choice(responses[i])
+        if predict_intent == "product_search":
+            category = fh.extract_category(user_input, nlp)
+            return fh.find_category(response, category)
+        
+        return response
+    else:
+        return "I can't answer this question. Please wait until the shop owner comes back!"
+
+# while True:
+#     user_input = input("Bạn: ")
+#     if user_input.lower() in ['exit', 'quit']:
+#         break
+#     response = get_response(user_input)
+#     print("Chatbot:", response)
 
 # API để nhận câu hỏi từ người dùng và trả về câu trả lời
 @app.route('/api/chat', methods=['POST'])
